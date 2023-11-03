c09a53e71ba8e988b21e7e310daae80d34d424e6
Sirori <116864776+Sirori@users.noreply.github.com>
2023-11-03 13:14:27 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,10 @@
+function solution(binomial) {
+    let arr = binomial.split(" ")
+    if(arr[1] === "+"){
+        return arr[0] * 1 + arr[2] * 1
+    }else if(arr[1] === "-"){
+        return arr[0] * 1 - arr[2] * 1
+    }else{
+        return arr[0] * 1 * arr[2] * 1
+    }
+}
